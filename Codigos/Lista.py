ea05536118e955054129efb7e08189db1a65b369
augusto <edgaraah126@hmail.com>
2026-04-22 09:38:58 -0500

    Trabajo con listas en Python

---
+++ Codigos/Lista.py
@@ -0,0 +1,33 @@
+
+'''
+LISTAS EN PYTHON
+
+Las listas son un conjunto de elementos ordenados y mutables que pueden contener datos de diferentes tipos, por ejemplo pueden contener números, cadenas de texto, otras listas, etc.
+
+Las listas se presentan por una serie de elementos separados por comas y delimitados entre corchetes [] .
+
+──▶ Sintaxis de una lista
+    Lista = [elemento1, elemento2, elemento3, ..., elementoN]
+
+──▶ Una lista de textos
+    frutas = ["manzana", "banana", "cereza"]
+
+──▶ Una lista mezclada
+    Lista = [56,"Python",3.14,[1,2,3],True]
+
+──▶ Una lista vacía (lista para ser llenada después)
+    mochila = []
+
+                 [  LISTA  ]
+    _________________
+   |  0  |  1  |  2  |  <-- Posición (Índice)
+   | [A] | [B] | [C] |  <-- Contenido (Valor)
+   |_____|_____|_____|
+      |     |     |
+      ▼     ▼     ▼
+
+En las listas, cada elemento tiene una posición o índice que comienza desde 0. Por ejemplo, en la lista frutas, "manzana" está en la posición 0, "banana" en la posición 1 y "cereza" en la posición 2.
+
+Indice Negativo: También se pueden usar índices negativos para acceder a los elementos desde el final de la lista. Por ejemplo, en la lista frutas, "cereza" se puede acceder con el índice -1, "banana" con el índice -2 y "manzana" con el índice -3.
+
+'''
